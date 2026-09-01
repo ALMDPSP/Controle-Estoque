a9499d6d435eb2c0c51b44875a7c53cccf706c91
ALMDPSP <167446841+ALMDPSP@users.noreply.github.com>
2026-09-01 19:14:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ from reportlab.pdfgen import canvas
 import db
 
 app = Flask(__name__)
-APP_BUILD = "2026-09-01-projecao-map-pdf-v8"
+APP_BUILD = "2026-09-01-footer-projecao-v9"
 app.secret_key = os.environ.get("SECRET_KEY", "troque-esta-chave-em-producao")
 app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
