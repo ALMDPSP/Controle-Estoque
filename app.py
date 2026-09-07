e8d5c5f202f542d3ee8f081df1d1fb10b181c16d
Alexandre Luiz Martins <167446841+ALMDPSP@users.noreply.github.com>
2026-09-07 14:55:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ import qrcode
 import db
 
 app = Flask(__name__)
-APP_BUILD = "2026-09-07-autenticacao-cinematica-v59"
+APP_BUILD = "2026-09-07-visual-aprovado-exato-v60"
 _DASHBOARD_CACHE = {"expira": 0.0, "dados": None}
 app.secret_key = os.environ.get("SECRET_KEY", "troque-esta-chave-em-producao")
 _RUNNING_HTTPS_HOSTED = bool(
